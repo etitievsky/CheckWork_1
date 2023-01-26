3bdcc2eddee1fce63abc2e3a3f4daf2809d1a73c
etitievsky <etitievsky@mail.ru>
2023-01-26 21:57:14 +0300

    Тело программы

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа из имеющегося массива строк формирует массив из строк, 
+// длина которых меньше, либо равна 3 символа
+
+
+
+
+
+
+string [] array = {"hello", "2", "world", ":-)"};
+string [] newArray = CreateNewArray(array);
+Console.Write($"[{String.Join(", ",newArray)}]");
