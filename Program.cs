8d70644ea72500e3c98a7deaa7f3d54f9faa6a25
etitievsky <etitievsky@mail.ru>
2023-01-27 07:10:48 +0300

    Добавлен метод CreateNewArray

--- Program.cs
+++ Program.cs
@@ -1,11 +1,27 @@
 ﻿// Программа из имеющегося массива строк формирует массив из строк, 
 // длина которых меньше, либо равна 3 символа
 
-
-
-
-
+string [] CreateNewArray(string [] array)
+{
+    int count = 0;
+    for(int i=0; i<array.Length; i++)
+    {
+        if(array[i].Length<=3) count++;
+    }
+    string [] newArray = new string [count];
+    int newArrayIndex = 0;
+    for(int i=0; i<array.Length; i++)
+    {
+        
+        if(array[i].Length<=3)
+        {
+            newArray[newArrayIndex] = array[i];
+            newArrayIndex++;
+        } 
+    }
+    return newArray;
+}
 
 string [] array = {"hello", "2", "world", ":-)"};
 string [] newArray = CreateNewArray(array);
-Console.Write($"[{String.Join(", ",newArray)}]");
+Console.Write($"[{String.Join(", ",array)}] -> [{String.Join(", ",newArray)}]");
